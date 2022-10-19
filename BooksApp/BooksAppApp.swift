0be6957eb0d73e2ffec67ba9f78f4e0b09418d61
Matt Mauler <matt@metasonic.studio>
2022-10-19 19:49:16 +0200

    Initial Commit

---
+++ BooksApp/BooksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksAppApp.swift
+//  BooksApp
+//
+//  Created by Matěj Mauler on 19.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct BooksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
